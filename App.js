444f3a1b8892052f07a7eae52519a65d40546a42
Orlando Sotelo <danugfe@gmail.com>
2020-04-26 15:04:54 -0500

    First hello component

--- App.js
+++ App.js
@@ -1,19 +1,17 @@
+//import stuff
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {View,Text} from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+//create stuff
+class App extends React.Component{
+  render(){
+    return(
+      <View>
+        <Text>Hello world.</Text>
+      </View>
+    )
+  }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+//export stuff
+export default App;
